c70bb39624eb8c384039f2b78822144a92c3cce6
Joachim Duong <joachimduong@gmail.com>
2026-01-25 20:56:15 +0100

    fixed syntax for engine

--- engine/Engine.cpp
+++ engine/Engine.cpp
@@ -1,20 +1,32 @@
 #include "Engine.h"
 #include <iostream>
 
-struct TickData {
-    const float TICK = 1.f / 60.f;
-    const float MAXFRAME = 0.25f;
-    float deltaTime = 0.f;
-    std::chrono::_V2::system_clock::time_point lastTimePulled = std::chrono::high_resolution_clock::now();
-    std::chrono::_V2::system_clock::time_point now = std::chrono::high_resolution_clock::now();
-};
+
 
 Engine::Engine() {
     running = true;
+    
+    
 }
 
 void Engine::run() {
     while(running) {
-        std::cout << "RUNNING";
+        tickData.now = std::chrono::high_resolution_clock::now();
+        tickData.deltaTime = time_to_float(tickData.now) - time_to_float(tickData.lastTimePulled);
+        if(tickData.deltaTime > tickData.MAXFRAME); tickData.deltaTime = tickData.MAXFRAME;
+
+
+        while(tickData.deltaTime > tickData.TICK) {
+
+            stateStack.top().update(tickData.TICK);
+            tickData.deltaTime - tickData.TICK;
+
+        }
     }
 }
+
+float Engine::time_to_float(std::chrono::system_clock::time_point time) {
+    return std::chrono::duration<float, std::milli>(time.time_since_epoch()).count();
+};
+
+
--- include/Engine.h
+++ include/Engine.h
@@ -1,6 +1,9 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 #include <chrono>
+#include <stack>
+#include "GameState.h"
+#include "TickData.h"
 
 
 class Engine {
@@ -8,8 +11,12 @@ class Engine {
     void run();
     Engine();
 
+
     private:
+    std::stack<GameState> stateStack;
     bool running;
+    TickData tickData;
+    float time_to_float(std::chrono::system_clock::time_point time);
 };
 
 #endif //ENGINE_H
